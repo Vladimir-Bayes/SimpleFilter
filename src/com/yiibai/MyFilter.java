3b4cf2d15c8167620f5666ee07f794758f771858
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-26 17:22:20 +0800

    Initial commit

---
+++ src/com/yiibai/MyFilter.java
@@ -0,0 +1,60 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+@WebFilter("/MyFilter")
+public class MyFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("过滤器调用之前<br/>");
+
+        chain.doFilter(request, response);// sends request to next resource
+
+        out.print("过滤器调用之后<br/>");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
